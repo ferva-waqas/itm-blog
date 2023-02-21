f9c8db73e4eec54b8f36adb4de1043f1398d07da
sidrazee <120570399+sidrazee@users.noreply.github.com>
2023-02-21 11:17:05 +0500

    comment route path id is now postId

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const commentController = require('../controllers/comment');
 const router = express.Router();
 
-router.post('/post/:id/comment', commentController.create);
+router.post('/post/:postId/comment', commentController.create);
 
 module.exports = router;
